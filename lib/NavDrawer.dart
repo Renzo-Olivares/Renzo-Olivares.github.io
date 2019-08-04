d294faf676606e84d441c63b90d6a32afc9f2a99
Renzo-Olivares <renzo.olivares1@gmail.com>
2019-08-04 15:57:56 -0700

    Add placeholders for multiple routes changed theming Added state to Navigation drawer

--- lib/NavDrawer.dart
+++ lib/NavDrawer.dart
@@ -1,16 +1,55 @@
 import 'package:flutter_web/material.dart';
+import 'package:personal_web_test/AboutRoute.dart';
+import 'package:personal_web_test/ProjectsRoute.dart';
+import 'package:personal_web_test/ExperienceRoute.dart';
+import 'package:personal_web_test/ResumeRoute.dart';
 
-class NavDrawer extends StatelessWidget {
+class NavDrawer extends StatefulWidget {
   NavDrawer({Key key}) : super(key: key);
+
+  @override
+  _NavDrawerState createState() => _NavDrawerState();
+}
+
+class _NavDrawerState extends State<NavDrawer> {
+  static int drawerPosition = 1;
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
+    List<List<dynamic>> drawerSections = [
+      [
+        "About",
+        Icons.person_outline,
+        1,
+        drawerPosition == 1,
+      ],
+      [
+        "Projects",
+        Icons.developer_mode,
+        2,
+        drawerPosition == 2,
+      ],
+      [
+        "Experience",
+        Icons.work,
+        3,
+        drawerPosition == 3,
+      ],
+      [
+        "Resume",
+        Icons.description,
+        4,
+        drawerPosition == 4,
+      ],
+    ];
+
     return Drawer(
       child: Column(
         children: <Widget>[
           SizedBox(
             height: 250,
             child: DrawerHeader(
+              margin: EdgeInsets.only(bottom: 0.0),
               decoration: BoxDecoration(
                 color: Theme.of(context).primaryColor,
               ),
@@ -30,7 +69,7 @@ class NavDrawer extends StatelessWidget {
                           shape: BoxShape.circle,
                           border: Border.all(
                             width: 4.0,
-                            color: Colors.blue[700],
+                            color: Colors.blue[500],
                           ),
                         ),
                         child: CircleAvatar(
@@ -66,27 +105,64 @@ class NavDrawer extends StatelessWidget {
             child: MediaQuery.removePadding(
               context: context,
               removeTop: true,
-              child: ListView(
-                padding: EdgeInsets.only(left: 10.0, right: 10.0),
-                children: <Widget>[
-                  ListTile(
-                    leading: Icon(Icons.person),
-                    title: Text("About"),
-                  ),
-                  ListTile(
-                    leading: Icon(Icons.developer_mode),
-                    title: Text("Projects"),
-                  ),
-                  ListTile(
-                    leading: Icon(Icons.description),
-                    title: Text("Resume"),
-                  ),
-                ],
+              child: Container(
+                color: Colors.blue[500],
+                child: ListView(
+                  //padding: EdgeInsets.only(left: 10.0, right: 10.0),
+                  children: drawerSections.map(createDrawerSections).toList(),
+                ),
               ),
             ),
           ),
         ],
       ),
     );
+  }
+
+  Widget createDrawerSections(List<dynamic> sections) {
+    return Card(
+      color: sections[3]? Colors.blue[600] : Colors.transparent,
+      elevation: sections[3]? 1.0: 0.0,
+      child: ListTile(
+        contentPadding: EdgeInsets.only(right: 30.0, left: 30.0),
+        onTap: () => setDrawerPosition(sections[2]),
+        leading: Icon(sections[1]),
+        title: Text(
+          sections[0],
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.w200),
+        ),
+      ),
+    );
+  }
+
+  void pushRoute() {
+    if (Navigator.of(context).canPop()) {
+      Navigator.of(context).pop();
+    }
+
+    Navigator.of(context).push(MaterialPageRoute(
+      builder: (BuildContext context) {
+        switch (drawerPosition) {
+          case 1:
+            return AboutRoute();
+          case 2:
+            return ProjectsRoute();
+          case 3:
+            return ExperienceRoute();
+          case 4:
+            return ResumeRoute();
+          default:
+            return AboutRoute();
+        }
+      },
+    ));
+  }
+
+  void setDrawerPosition(int newPosition) {
+    setState(() {
+      drawerPosition = newPosition;
+    });
+
+    pushRoute();
   }
 }
--- lib/ProjectsRoute.dart
+++ lib/ProjectsRoute.dart
@@ -1,19 +1,16 @@
 import 'package:flutter_web/material.dart';
 import 'package:personal_web_test/NavDrawer.dart';
 
-class HomeMobile extends StatelessWidget {
-  String brand;
-
-  HomeMobile({Key key, this.brand}) : super(key: key);
-
+class ProjectsRoute extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // TODO: implement build
     return Scaffold(
       drawer: NavDrawer(),
       appBar: AppBar(
-        elevation: 1.0,
-        title: Text(brand),
+        title: Text("Renzo Olivares"),
       ),
+      body: Text("Projects"),
     );
   }
 }
