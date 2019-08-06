a08d973e7937fb0f210499834cdb197e2187b4e0
Renzo-Olivares <renzo.olivares1@gmail.com>
2019-08-06 14:58:25 -0700

    add padding between cards and between header

--- lib/Widgets/InfoCard.dart
+++ lib/Widgets/InfoCard.dart
@@ -11,25 +11,28 @@ class InfoCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 4.0,
-      color: Theme.of(context).accentColor,
-      child: Row(
-        children: <Widget>[
-          Card(
-            elevation: 2.0,
-            child: Padding(
-              padding: padding
-                  ? const EdgeInsets.all(2.0)
-                  : const EdgeInsets.all(0.0),
-              child: ImageIcon(icon, size: 96.0),
+    return Padding(
+      padding: const EdgeInsets.only(top: 10.0),
+      child: Card(
+        elevation: 4.0,
+        color: Theme.of(context).accentColor,
+        child: Row(
+          children: <Widget>[
+            Card(
+              elevation: 2.0,
+              child: Padding(
+                padding: padding
+                    ? const EdgeInsets.all(2.0)
+                    : const EdgeInsets.all(0.0),
+                child: ImageIcon(icon, size: 96.0),
+              ),
             ),
-          ),
-          Card(
-            elevation: 2.0,
-            child: Text(info),
-          ),
-        ],
+            Card(
+              elevation: 2.0,
+              child: Text(info),
+            ),
+          ],
+        ),
       ),
     );
   }
