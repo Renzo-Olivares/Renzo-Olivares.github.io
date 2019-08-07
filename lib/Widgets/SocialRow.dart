8cf793f0c59c648387cbdde7a22cb02def863a0b
Renzo-Olivares <renzo.olivares1@gmail.com>
2019-08-06 20:09:51 -0700

    Revert " updates"
    This reverts commit d8997b61443aa5bb723f50202f2eac2605593e86.

--- lib/Widgets/SocialRow.dart
+++ lib/Widgets/SocialRow.dart
@@ -10,7 +10,7 @@ class SocialRow extends StatelessWidget {
     List<List<dynamic>> socialSites = [
       [
         AssetImage('images/github-logo.png'),
-        'renzo.olivares1@gmail.com',
+        'https://github.com/Renzo-Olivares',
         'Github',
       ],
       [
