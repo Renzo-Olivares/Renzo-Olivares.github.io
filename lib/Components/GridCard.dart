c9d112d6ba19148ff5191afe268960848d62e776
Renzo <renzo.olivares1@gmail.com>
2020-07-19 19:38:26 -0700

    Add ripple to project card

--- lib/Components/GridCard.dart
+++ lib/Components/GridCard.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 
 class GridCard extends StatelessWidget {
   const GridCard({this.darkPreview, this.lightPreview, this.openChild});
+
   final String darkPreview;
   final String lightPreview;
   final Widget openChild;
@@ -17,11 +18,13 @@ class GridCard extends StatelessWidget {
       openBuilder: (context, openContainer) => openChild,
       closedColor: Theme.of(context).bottomAppBarColor,
       closedBuilder: (context, openContainer) {
-        return InkWell(
-          onTap: openContainer,
-          child: Image.asset(
+        return Ink.image(
+          image: AssetImage(
             isDark ? darkPreview : lightPreview,
-            fit: BoxFit.fill,
+          ),
+          fit: BoxFit.fill,
+          child: InkWell(
+            onTap: openContainer,
           ),
         );
       },
